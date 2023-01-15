3e267f3709d59e3826000422de4a29d08807caab
manuelmad <manuelmad1981@gmail.com>
2023-01-15 17:58:41 -0400

    Agregando al Web Component del header los links y aplicándolo a todas las vistas de mi página web

--- docs/components/my-header.js
+++ docs/components/my-header.js
@@ -4,11 +4,26 @@ class customHeader extends HTMLElement {
         this.attachShadow({mode: "open"});
     }
     static get observedAttributes() {
-        return ["img", "link1", "link2", "link3", "link4"];
+        return ["img", "name1", "name2", "name3", "name4","linkHome", "link1", "link2", "link3", "link4"];
     }
     attributeChangedCallback(attr, oldVal, newVal) {
         if(attr === "img") {
             this.img = newVal;
+        }
+        if(attr === "name1") {
+            this.name1 = newVal;
+        }
+        if(attr === "name2") {
+            this.name2 = newVal;
+        }
+        if(attr === "name3") {
+            this.name3 = newVal;
+        }
+        if(attr === "name4") {
+            this.name4 = newVal;
+        }
+        if(attr === "linkHome") {
+            this.linkHome = newVal;
         }
         if(attr === "link1") {
             this.link1 = newVal;
@@ -28,15 +43,15 @@ class customHeader extends HTMLElement {
         template.innerHTML = `
             <header>
                 <div class="header-logo-container">
-                    <a href="./index.html"><img src="${this.img}" alt="logo"></a>
+                    <a href="${this.linkHome}"><img src="${this.img}" alt="logo"></a>
                 </div>
                 <nav>
                     <div class="container__menu-principal">
                         <ul id="menu-principal">
-                            <li><a href="./proyectos/proyectos.html">${this.link1}</a></li>
-                            <li><a href="#personal-info">${this.link2}</a></li>
-                            <li><a href="#curriculum">${this.link3}</a></li>
-                            <li><a href="#contacto">${this.link4}</a></li>
+                            <li><a href="${this.link1}">${this.name1}</a></li>
+                            <li><a href="${this.link2}">${this.name2}</a></li>
+                            <li><a href="${this.link3}">${this.name3}</a></li>
+                            <li><a href="${this.link4}">${this.name4}</a></li>
                         </ul>
                     </div>
                     <div class="container__icono-menu">
@@ -45,32 +60,32 @@ class customHeader extends HTMLElement {
                         </svg>
                         <ul id="menu-desplegable">
                             <li>
-                                <a href="./proyectos/proyectos.html">
+                                <a href="${this.link1}">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder2-open" viewBox="0 0 16 16">
                                         <path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v.64c.57.265.94.876.856 1.546l-.64 5.124A2.5 2.5 0 0 1 12.733 15H3.266a2.5 2.5 0 0 1-2.481-2.19l-.64-5.124A1.5 1.5 0 0 1 1 6.14V3.5zM2 6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5a.5.5 0 0 0-.5.5V6zm-.367 1a.5.5 0 0 0-.496.562l.64 5.124A1.5 1.5 0 0 0 3.266 14h9.468a1.5 1.5 0 0 0 1.489-1.314l.64-5.124A.5.5 0 0 0 14.367 7H1.633z"/>
-                                    </svg> ${this.link1}
+                                    </svg> ${this.name1}
                                 </a>
                             </li>
                             <li>
-                                <a href="#personal-info">
+                                <a href="${this.link2}">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                                         <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-                                    </svg> ${this.link2}
+                                    </svg> ${this.name2}
                                 </a>
                             </li>
                             <li>
-                                <a href="#curriculum">
+                                <a href="${this.link3}">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-person" viewBox="0 0 16 16">
                                         <path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                                         <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2v9.255S12 12 8 12s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h5.5v2z"/>
-                                    </svg> ${this.link3}
+                                    </svg> ${this.name3}
                                 </a>
                             </li>
                             <li>
-                                <a href="#contacto">
+                                <a href="${this.link4}">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
                                         <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/>
-                                    </svg> ${this.link4}
+                                    </svg> ${this.name4}
                                 </a>
                             </li>
                         </ul>
